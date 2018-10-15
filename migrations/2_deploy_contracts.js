2b948fbc873f9cd4fd0e9a02cddc889e748e0520
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2018-10-15 18:21:48 +1100

    Add linting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var NetworkToken = artifacts.require("./NetworkToken.sol");
+const NetworkToken = artifacts.require('./NetworkToken.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(NetworkToken);
-};
+  deployer.deploy(NetworkToken)
+}
--- test/NetworkToken.test.js
+++ test/NetworkToken.test.js
@@ -1,98 +1,96 @@
-const chai = require('chai');
-const expect = chai.expect;
-const BN = require('bn.js');
-const bnChai = require('bn-chai');
-chai.use(bnChai(BN));
+const chai = require('chai')
+const expect = chai.expect
+const BN = require('bn.js')
+const bnChai = require('bn-chai')
+chai.use(bnChai(BN))
 
-const NetworkToken = artifacts.require('NetworkToken');
+const NetworkToken = artifacts.require('NetworkToken')
 
-contract('NetworkToken', function ([owner, anotherAccount]) {
-  
-  let token;
+contract('NetworkToken', function([owner, anotherAccount]) {
+  let token
 
-  const _name = 'Network Token (Pilot)';
-  const _symbol = 'NT';
-  const _decimals = 18;
-  var _cap = (new BN(100 * (10 ** 9))).mul((new BN(10 ** 9)).sqr());
-  
-  beforeEach(async function () {
-    token = await NetworkToken.new({ from: owner });
-  });
+  const _name = 'Network Token (Pilot)'
+  const _symbol = 'NT'
+  const _decimals = 18
+  const _cap = new BN(100 * 10 ** 9).mul(new BN(10 ** 9).sqr())
 
-  describe('token details', function () {
-    it('has a name', async function () {
-      const name = await token.name();
-      assert.equal(name, _name);
-    });
+  beforeEach(async function() {
+    token = await NetworkToken.new({ from: owner })
+  })
 
-    it('has a symbol', async function () {
-      const symbol = await token.symbol();
-      assert.equal(symbol, _symbol);
-    });
+  describe('token details', function() {
+    it('has a name', async function() {
+      const name = await token.name()
+      assert.equal(name, _name)
+    })
 
-    it('has 18 decimals', async function () {
-      const decimals = await token.decimals();
-      expect(decimals).to.eq.BN(_decimals);
-    });
+    it('has a symbol', async function() {
+      const symbol = await token.symbol()
+      assert.equal(symbol, _symbol)
+    })
 
-    it('has a 0 total supply', async function () {
-      const totalSupply = await token.totalSupply();
-      expect(totalSupply).to.eq.BN(0);
-    });
-  
-    it('has a 0 owner supply', async function () {
-      const ownerBalance = await token.balanceOf(owner);
-      expect(ownerBalance).to.eq.BN(0);
-    });
-    
-    it('has the cap set', async function () {
-      const tokenCap = await token.cap();
-      expect(tokenCap).to.eq.BN(_cap);
-    });
-  });
+    it('has 18 decimals', async function() {
+      const decimals = await token.decimals()
+      expect(decimals).to.eq.BN(_decimals)
+    })
 
-  describe('token minting', function () {
-    const amount = 100;
-    const from = owner;
+    it('has a 0 total supply', async function() {
+      const totalSupply = await token.totalSupply()
+      expect(totalSupply).to.eq.BN(0)
+    })
 
-    it('can mint to owner', async function () {
-      await token.mint(owner, amount, { from });
+    it('has a 0 owner supply', async function() {
+      const ownerBalance = await token.balanceOf(owner)
+      expect(ownerBalance).to.eq.BN(0)
+    })
 
-      const balance = await token.balanceOf(owner);
-      expect(balance).to.eq.BN(amount);
-    });
+    it('has the cap set', async function() {
+      const tokenCap = await token.cap()
+      expect(tokenCap).to.eq.BN(_cap)
+    })
+  })
 
-    it('can mint to another account', async function () {
-      await token.mint(anotherAccount, amount, { from });
+  describe('token minting', function() {
+    const amount = 100
+    const from = owner
 
-      const balance = await token.balanceOf(anotherAccount);
-      expect(balance).to.eq.BN(amount);
-    });
+    it('can mint to owner', async function() {
+      await token.mint(owner, amount, { from })
 
-    it('cannot mint after minting stopped', async function () {
-      await token.finishMinting({ from});
+      const balance = await token.balanceOf(owner)
+      expect(balance).to.eq.BN(amount)
+    })
 
-      try {
-        await token.mint(owner, amount, { from });
-        assert.fail('Expected revert not received');
-      } catch (error) {
-        //assert(error.reason === '[No reason implemented]');
-        assert(error.message.includes(''));
-      }
-    });
+    it('can mint to another account', async function() {
+      await token.mint(anotherAccount, amount, { from })
 
-    it('cannot mint above cap', async function () {
-      const tokenCap = await token.cap();
-      await token.mint(owner, tokenCap, { from });
+      const balance = await token.balanceOf(anotherAccount)
+      expect(balance).to.eq.BN(amount)
+    })
+
+    it('cannot mint after minting stopped', async function() {
+      await token.finishMinting({ from })
 
       try {
-        await token.mint(owner, amount, { from });
-        assert.fail('Expected revert not received');
+        await token.mint(owner, amount, { from })
+        assert.fail('Expected revert not received')
       } catch (error) {
         //assert(error.reason === '[No reason implemented]');
-        assert(error.message.includes(''));
+        assert(error.message.includes(''))
       }
-    });
+    })
 
-  });
-});
+    it('cannot mint above cap', async function() {
+      const tokenCap = await token.cap()
+      await token.mint(owner, tokenCap, { from })
+
+      try {
+        await token.mint(owner, amount, { from })
+        assert.fail('Expected revert not received')
+      } catch (error) {
+        //assert(error.reason === '[No reason implemented]');
+        assert(error.message.includes(''))
+      }
+    })
+  })
+})
